cdbf3d6ee04609533ce9b5b260a834919b87752a
ectopunk <ectopunk@users.noreply.github.com>
2017-04-13 17:28:28 -0700

    Added rsync example

--- file_move.sh
+++ file_move.sh
@@ -1,1 +1,3 @@
-move dat file
+/bin/date >> /home/multiverse/logs/rsync.log
+/usr/bin/rsync -e ssh -auv --exclude-from=/home/multiverse/bin/rsync_exclusions.txt multiverse@multiverse.org:~/ /home/multiverse/website/ >> /home/multiverse/logs/rsync.log 2>&1
+/bin/date >> /home/multiverse/logs/rsync.log
